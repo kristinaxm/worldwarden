615181082c54939683682b55888cd81b2aaf05c2
Adam Ottosson <addeecsgo@gmail.com>
2026-09-22 11:18:27 +0200

    Adds footer with menu links

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <a href="#">Help</a>
+    <a href="#">Privacy</a>
+    <a href="#">Terms</a>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  display: flex;
+  justify-content: space-evenly;
+  gap: 16px;
+  font-size: 14px;
+  color: var(--color-text-secondary);
+}
+
+.footer a:hover {
+  text-decoration: underline;
+}
+</style>
